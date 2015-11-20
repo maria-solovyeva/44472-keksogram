7241c0ff6c8b51e93271c3eb9e1b77e0b0fbb8c9
maria-solovyeva <mv.solovyeva@yandex.ru>
2015-11-21 00:34:49 +0700

    module1-task2-additions

--- js/check.js
+++ js/check.js
@@ -1,21 +1,31 @@
 ﻿function getMessage(a, b) {
-	return string;
-	if (a=boolean) {
-		var a=true;
-		if (a) {
-			"Переданное GIF-изображение анимировано и содержит [b] кадров";
+	if (typeof a === 'boolean') {
+		if (a===true) {
+			return  'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
 		}
 		else {
-			"Переданное GIF-изображение не анимировано";
+			return 'Переданное GIF-изображение не анимировано';
 		}
 	}
-	if (a=number) {
-		"Переданное SVG-изображение содержит [a] объектов и [b * 4] аттрибутов"
+	if (typeof a === 'number') {
+		return 'Переданное SVG-изображение содержит ' + a + ' объектов и '  + b * 4 + ' аттрибутов';
 	}
-	if (a=array) {
-		"Количество красных точек во всех строчках изображения: [sum]"
+	if (Array.isArray(a)) {
+		var sumArray = 0;
+		
+		for (var i = 0; i < a.length; i++) {
+			sumArray += a[i];
+		}
+		return 'Количество красных точек во всех строчках изображения: ' + sumArray;
+		
 	}
-	if (a=array, b=array) {
-		"Общая площадь артефактов сжатия: 165 пикселей"
+	if (Array.isArray(a) && Array.isArray(b)) {
+		var squareArray = 0;
+		
+		for (var i = 0; i < a.length; i++) {
+			squareArray += a[i] * b[i];
+		}
+		
+		return 'Общая площадь артефактов сжатия: ' + squareArray + ' пикселей';
 	}
 }
