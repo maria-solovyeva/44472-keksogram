b8c3ef2715af133ec96bdf2b545d8db3b38683dc
maria-solovyeva <mv.solovyeva@yandex.ru>
2015-12-15 22:10:31 +0700

    module5-task1

--- js/pictures.js
+++ js/pictures.js
@@ -1,18 +1,93 @@
 'use strict';
 
 (function() {
-  var filters = document.querySelector('.filters');
   var container = document.querySelector('.pictures');
+  var template = document.querySelector('#picture-template');
+  var activeFilter = 'filter-popular';
+  var pictures = [];
 
-  filters.classList.add('hidden');
+  var filters = document.querySelectorAll('.filters-radio');
+  for (var i = 0; i < filters.length; i++) {
+    filters[i].onclick = function(evt) {
+      var clickedElementID = evt.target.id;
+      setActiveFilter(clickedElementID);
+    };
+  }
 
-  window.pictures.forEach(function(picture) {
-    var element = getElementFromTemplate(picture);
-    container.appendChild(element);
-  });
+  container.classList.add('pictures-loading');
+
+  getPictures();
+
+  function renderPictures(picturesToRender) {
+    container.innerHTML = '';
+    var fragment = document.createDocumentFragment();
+
+    picturesToRender.forEach(function(picture) {
+      var element = getElementFromTemplate(picture);
+      
+      fragment.appendChild(element);
+    });
+
+    container.appendChild(fragment);
+  }
+
+  function setActiveFilter(id) {
+    if (activeFilter === id) {
+      return;
+    }
+
+    var filteredPictures = pictures.slice(0);
+
+    switch (id) {
+      case 'filter-new':
+        filteredPictures = filteredPictures.sort(function(a, b) {
+          var realADate = new Date(a.date);
+          var realBDate = new Date(b.date);
+          var timestampA = realADate.getTime();
+          var timestampB = realBDate.getTime();
+          return timestampB - timestampA;
+        });
+        break;
+      case 'filter-discussed':
+        filteredPictures = filteredPictures.sort(function(a, b) {
+          return b.comments - a.comments;
+        });
+        break;
+    }
+
+    renderPictures(filteredPictures);
+
+    activeFilter = id;
+  }
+
+  function getPictures() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'data/pictures.json');
+    xhr.onload = function(evt) {
+      var rawData = evt.target.response;
+      var loadedPictures = JSON.parse(rawData);
+      updateLoadedPictures(loadedPictures);
+    };
+    xhr.onerror = function() {
+      picturesFailure();
+    };
+    xhr.timeout = 10000;
+    xhr.ontimeout = function() {
+      picturesFailure();
+    };
+    xhr.send();
+  }
+
+  function updateLoadedPictures(loadedPictures) {
+    pictures = loadedPictures;
+    setActiveFilter(activeFilter, true);
+  }
+
+  function picturesFailure() {
+    container.classList.add('pictures-failure');
+  }
 
   function getElementFromTemplate(data) {
-    var template = document.querySelector('#picture-template');
     var element;
     if ('content' in template) {
       element = template.content.children[0].cloneNode(true);
@@ -23,8 +98,8 @@
     element.querySelector('.picture-comments').textContent = data.comments;
     element.querySelector('.picture-likes').textContent = data.likes;
 
-    var image = new Image(182, 182);
-
+    var image = new Image();
+    var imageSize = 182;
     var IMAGE_TIMEOUT = 10000;
 
     var imageLoadTimeout = setTimeout(function() {
@@ -42,8 +117,10 @@
     };
 
     image.src = data.url;
+    image.width = imageSize;
+    image.height = imageSize;
 
     return element;
   }
-  filters.classList.remove('hidden');
+  container.classList.remove('pictures-loading');
 })();
